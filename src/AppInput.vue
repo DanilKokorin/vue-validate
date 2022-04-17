51961e2256b3452301e2f67383b4d55e2c902fc4
Danil Kokorin <63406852+DanilKokorin@users.noreply.github.com>
2022-04-17 22:18:43 +0300

    Add files via upload

---
+++ src/AppInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="form-control" :class="{invalid: errors}">
+        <label for="id">{{ label }}</label>
+        <input 
+            type="text" 
+            :id="id" 
+            :placeholder="placeholder"
+            :value="modelValue"
+            @input="onChange"
+        >
+        <small v-if="errors">{{ errors }}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ['update:modelValue'],
+    props: {
+        modelValue: String,
+        placeholder: String,
+        label: String,
+        errors: String
+    },
+    methods: {
+        onChange (event) {
+            this.$emit('update:modelValue', event.target.value)
+        }
+    },
+    data () {
+        return {
+            id: 'input-' + Math.random()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
